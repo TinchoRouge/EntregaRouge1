7b0ed679fdd115b6b14f7557844e9fbaf71673ba
TinchoRouge <rougemartintomas@gmail.com>
2022-06-08 01:55:42 -0300

    pagina de inicio

--- proyecto_coder/urls.py
+++ proyecto_coder/urls.py
@@ -18,5 +18,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("app_coder/" , include("app_coder.urls"))
+    path("app_coder/" , include("app_coder.urls")),
+    path("" , include("app_coder.urls"))
 ]
